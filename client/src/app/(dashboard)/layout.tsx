b4f0a4de16ea7081025c6510955bb4e5603f505b
Najib DOUKKANI <najib.doukkani@gmail.com>
2025-01-06 15:52:35 +0100

    course view page

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import AppSidebar from "@/components/AppSidebar";
 import Loading from "@/components/Loading";
@@ -7,14 +7,25 @@ import { SidebarProvider } from "@/components/ui/sidebar";
 import { cn } from "@/lib/utils";
 import { useUser } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ChaptersSidebar from "./user/courses/[courseId]/ChaptersSidebar";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const pathname = usePathname();
   const [courseId, setCourseId] = useState<string | null>(null);
   const { user, isLoaded } = useUser();
+  const isCoursePage = /^\/user\/courses\/[^\/]+(?:\/chapters\/[^\/]+)?$/.test(
+    pathname
+  );
 
-  //handle useEffect isCoursePage
+  useEffect(() => {
+    if (isCoursePage) {
+      const match = pathname.match(/\/user\/courses\/([^\/]+)/);
+      setCourseId(match ? match[1] : null);
+    } else {
+      setCourseId(null);
+    }
+  }, [isCoursePage, pathname]);
 
   if (!isLoaded) return <Loading />;
   if (!user) return <div>Please sign in to access this page.</div>;
@@ -24,9 +35,15 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="dashboard">
         <AppSidebar />
         <div className="dashboard__content">
-          {/* chapters sidebar will go here */}
-          <div className={cn("dashboard__main")} style={{ height: "100vh" }}>
-            <Navbar />
+          {courseId && <ChaptersSidebar />}
+          <div
+            className={cn(
+              "dashboard__main",
+              isCoursePage && "dashboard__main--not-course"
+            )}
+            style={{ height: "100vh" }}
+          >
+            <Navbar isCoursePage={isCoursePage} />
             <main className="dashboard__body">{children}</main>
           </div>
         </div>
